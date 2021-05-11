d5c71301d9c6b9b40ebbc4fab9b7f06a2cd9d194
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-05-11 11:17:50 +0200

    JS-ASIN-PELICULAS-PARTE2

--- JS - Asincrono/Peliculas/script.js
+++ JS - Asincrono/Peliculas/script.js
@@ -2,16 +2,62 @@ const key = "e39857b2e071fb83dba7d82258147a51&language=es-ES";
 const keyPopularMovies =
   "https://api.themoviedb.org/3/movie/popular?api_key=e39857b2e071fb83dba7d82258147a51&language=es-ES";
 
-const renderMovies = (movies) => {
-  for (const movie of movies) {
-    document.querySelector("main.movies").innerHTML += `
-    <div class="movie">
-        <h2>${movie.title}</h2>
-        <img src="http://image.tmdb.org/t/p/w185${movie.poster_path}">
+const getMovieHtml = (movie) => {
+  return `
+    <div class="movie" onclick="getMovieDetailed(${movie.id})">
+      <img src="http://image.tmdb.org/t/p/w185${movie.poster_path}"alt="imagen de la pelicula">
+      <h2>${movie.title}</h2>
     </div>`;
+};
+
+const getMovieDetailedHtml = (movie) => {
+  return `
+    <div class="movie" onclick="getMovieDetailed(${movie.id})">
+      <img src="http://image.tmdb.org/t/p/w300${movie.poster_path}" alt="imagen de la pelicula">
+      <h2>${movie.title}</h2>
+      <span>Popularidad: ${movie.popularity}</span>
+      <p>Descripción: ${movie.overview}</p>
+      
+    </div>`;
+};
+
+const renderMovies = (movies) => {
+  document.querySelector("main.movies").innerHTML = ""; // Limpia el HTML
+  for (const movie of movies) {
+    document.querySelector("main.movies").innerHTML += getMovieHtml(movie);
   }
 };
 
+// Con PROMESAS
+const getMovieDetailed = (movie_id) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${movie_id}?api_key=e39857b2e071fb83dba7d82258147a51&language=es-ES`
+    )
+    .then((res) => {
+      const movie = res.data;
+      document.querySelector("main.movies").innerHTML =
+        getMovieDetailedHtml(movie);
+    })
+    .catch(console.error);
+};
+
+// con ASYNC AWAIT
+const getLatestMovies = async () => {
+  try {
+    const res = await axios.get(
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=e39857b2e071fb83dba7d82258147a51&language=es-ES"
+    );
+
+    const movies = res.data.results;
+    renderMovies(movies);
+  } catch (error) {
+    console.error();
+    error;
+  }
+};
+
+// con PROMESAS
 const getPopularMovies = () => {
   fetch(keyPopularMovies)
     .then((res) => res.json())
